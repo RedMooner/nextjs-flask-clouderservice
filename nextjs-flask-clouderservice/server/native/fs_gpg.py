5667ba42de8b973519e213abca6df4c226a3cc48
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2023-10-02 14:15:46 +0500

    correction of the output of the list of files and folders in the directory

--- nextjs-flask-clouderservice/server/native/fs_gpg.py
+++ nextjs-flask-clouderservice/server/native/fs_gpg.py
@@ -69,16 +69,14 @@ def get_files_and_folders(foldername="", mail=""):
         "files": [],
         "folders": []
     }
-    print ("../uploads/users/" + mail + "/" + foldername)
-    for root, dirs, files in os.walk("uploads/users/" + mail + "/" + foldername):
-        # Получаем список файлов
-        for file_name in files:
-            data["files"].append(os.path.join(root, file_name))
-
-        # Получаем список папок
-        for dir_name in dirs:
-            data["folders"].append(os.path.join(root, dir_name))
-
+    folder_path = os.path.join("uploads", "users", mail, foldername)
+    if os.path.isdir(folder_path):
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path):
+                data["files"].append(file_path)
+            elif os.path.isdir(file_path):
+                data["folders"].append(file_path)
     return data
 
 
