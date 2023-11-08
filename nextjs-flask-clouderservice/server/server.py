f7ee65ec2151afb868aa16dc1eca4045b5dde86e
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2023-11-08 11:51:56 +0500

    comments to server code

--- nextjs-flask-clouderservice/server/server.py
+++ nextjs-flask-clouderservice/server/server.py
@@ -43,17 +43,24 @@ def home():
 
 @app.route("/api/register", methods=['POST'])
 def register():
+    # Get the data from the request
     Name = request.json['Name']
     Surname = request.json['Surname']
     Email = request.json['Email']
     Password = request.json['Password']
+    # Create a new user
     user = User(Name, Email, Password)
+    # Add the user to the session
     session.add(user)
+    # Commit the changes to the session
     session.commit()
+    # Generate a token for the user
     token = user.get_token()
+    # Generate a key for the user
     generate_key(Name, Surname, "clouder.com", Password)
     print("key generated")
     print(token)
+    # Return the token
     return jsonify(
         {
             'token': token
@@ -62,43 +69,59 @@ def register():
 
 @app.route("/api/logout", methods=['POST'])
 def logout():
+    # This route is used to logout the user
     pass
 
 
 @app.route("/api/login", methods=['POST'])
 def login():
     try:
+        # Get the username and password from the request
         username = request.json['username']
         password = request.json['password']
+        # Authenticate the user
         user = User.authenticate(username, password)
+        # Generate a token for the user
         token = user.get_token()
         print(token)
+        # Return the token
         return jsonify(
             {
                 'status': token
             })
+    # If the credentials are invalid
     except:
+        # Return an error message
         return jsonify(
             {
                 'status': "Invalid Credentials"
             })
-
 # cloude service endpoints
 
 
 @app.route('/api/download/<string:filename>', methods=['GET'])
+# Decorator to check if a JWT token is provided
 @jwt_required()
 def downloadFile(filename):
+    # Get the user ID from the JWT token
     user_id = get_jwt_identity()
+    # Get the user object from the database
     user = User.query.filter(User.id == user_id).one()
+    # Create the path to the file
     folder_path = os.path.join("uploads", "users", user.email, filename)
+    # Print the path to the file
     print(folder_path)
+    # Return the file as an attachment
     return send_file(folder_path, as_attachment=True)
-
 
 @app.route("/api/getfiles/<string:path>", methods=['GET'])
 @jwt_required()
 def getfiles(path):
+    '''
+    GET: /api/getfiles/<path>
+
+    Returns a list of files and folders in the given path.
+    '''
     user_id = get_jwt_identity()
     user = User.query.filter(User.id == user_id).one()
     if (path == 'root'):
@@ -130,20 +153,26 @@ def upload_file():
 @app.route("/api/profile", methods=['GET'])
 @jwt_required()
 def profile():
+    # Get the user id from the jwt token
     user_id = get_jwt_identity()
+    # Query the user from the database
     user = User.query.filter(User.id == user_id).one()
+    # Create a serialized user object
     serialized_user = [
         {
             'id': user.id,
             'email': user.email,
         }
     ]
+    # Print the serialized user object
     print(str(serialized_user))
+    # Return the serialized user object as a json
     return jsonify(serialized_user)
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
+    # Remove the session when the app context is shutdown
     session.remove()
 
 
